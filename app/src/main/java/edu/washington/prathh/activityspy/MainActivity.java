75cf25dd85131fcebdc1aadb62d15f6ba523395e
Hillary Prather <prathh@uw.edu>
2015-01-20 10:06:30 -0800

    Added logging behavior

--- app/src/main/java/edu/washington/prathh/activityspy/MainActivity.java
+++ app/src/main/java/edu/washington/prathh/activityspy/MainActivity.java
@@ -4,14 +4,17 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.Log;
 
 
 public class MainActivity extends ActionBarActivity {
+    private static final String ACTIVITY = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(ACTIVITY, "onCreate even fired");
     }
 
 
@@ -19,6 +22,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        Log.i(ACTIVITY, "onCreateOptionsMenu event fired");
         return true;
     }
 
@@ -35,5 +39,11 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.e(ACTIVITY, "We're going down, Captain!");
     }
 }
