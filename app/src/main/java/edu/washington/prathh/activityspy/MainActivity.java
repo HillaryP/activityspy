bee5814deca02db862652cfa3222899c34ac626b
HillaryP <prathh@uw.edu>
2015-01-20 14:41:43 -0800

    fixed merge conflicts

--- app/src/main/java/edu/washington/prathh/activityspy/MainActivity.java
+++ app/src/main/java/edu/washington/prathh/activityspy/MainActivity.java
@@ -6,10 +6,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.util.Log;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 8c75932949e1b7bbca4bb641a360736147c4836e
 public class MainActivity extends ActionBarActivity {
     private static final String ACTIVITY = "MainActivity";
 
@@ -17,11 +13,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
         Log.i(ACTIVITY, "onCreate event fired");
-=======
-        Log.i(ACTIVITY, "onCreate even fired");
->>>>>>> 8c75932949e1b7bbca4bb641a360736147c4836e
     }
 
 
@@ -29,10 +21,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-<<<<<<< HEAD
-=======
         Log.i(ACTIVITY, "onCreateOptionsMenu event fired");
->>>>>>> 8c75932949e1b7bbca4bb641a360736147c4836e
         return true;
     }
 
@@ -53,10 +42,7 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public void onDestroy() {
-<<<<<<< HEAD
-=======
         super.onDestroy();
->>>>>>> 8c75932949e1b7bbca4bb641a360736147c4836e
         Log.e(ACTIVITY, "We're going down, Captain!");
     }
 }
